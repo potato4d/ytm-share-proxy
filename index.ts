f24ee16eca2e043b87ae27ec8b4070773a5c5f18
Takuma HANATANI <mail@potato4d.me>
2021-01-31 15:51:24 +0900

    chore: enable dynamic useragent

--- index.ts
+++ index.ts
@@ -8,8 +8,7 @@ const { JSDOM } = jsdom
 
 const apiClient = axios.create({
   headers: {
-    "User-Agent":
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+    "User-Agent": `${process.env.YTM_USER_AGENT}`
   },
 });
 
